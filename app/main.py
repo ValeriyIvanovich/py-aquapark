01856eaf9f227cd73e5dff0d7852136c2577565a
Yaroslav Ivanov <extazzzyprofit@gmail.com>
2024-04-06 17:02:43 +0300

    solution py-aquapark

--- app/main.py
+++ app/main.py
@@ -1,25 +1,73 @@
+from __future__ import annotations
 from abc import ABC
+from typing import Callable
 
 
 class IntegerRange:
-    pass
+
+    def __init__(self, min_amount: int, max_amount: int) -> None:
+        self.min_amount = min_amount
+        self.max_amount = max_amount
+
+    def __set_name__(self, owner, name: str) -> None: # noqa
+        self.protected_name = "_" + name
+
+    def __get__(self, instance, owner) -> int: # noqa
+        return getattr(instance, self.protected_name)
+
+    def __set__(self, instance, value): # noqa
+        if self.min_amount <= value <= self.max_amount:
+            setattr(instance, self.protected_name, True)
+        else:
+            setattr(instance, self.protected_name, False)
 
 
 class Visitor:
-   pass
+
+    def __init__(self,
+                 name: str,
+                 age: int,
+                 weight: int,
+                 height: int) -> None:
+        self.name = name
+        self.age = age
+        self.weight = weight
+        self.height = height
 
 
 class SlideLimitationValidator(ABC):
-    pass
+
+    def __init__(self,
+                 age: int,
+                 weight: int,
+                 height: int) -> None:
+        self.age = age
+        self.weight = weight
+        self.height = height
 
 
 class ChildrenSlideLimitationValidator(SlideLimitationValidator):
-    pass
+    age = IntegerRange(4, 14)
+    weight = IntegerRange(80, 120)
+    height = IntegerRange(20, 50)
 
 
 class AdultSlideLimitationValidator(SlideLimitationValidator):
-    pass
+    age = IntegerRange(14, 60)
+    weight = IntegerRange(120, 220)
+    height = IntegerRange(50, 120)
 
 
 class Slide:
-    pass
+
+    def __init__(self,
+                 name: str,
+                 limitation_class: Callable) -> None:
+        self.name = name
+        self.limitation_class = limitation_class
+
+    def can_access(self, visitor: Visitor) -> bool:
+        check_limitations = self.limitation_class(visitor.age,
+                                                  visitor.height,
+                                                  visitor.weight)
+        return all(check_limitations.__dict__.values())
