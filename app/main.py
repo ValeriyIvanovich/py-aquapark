850fb532575aa86af2170208e8bd3386c073bc3e
Artem Andriushchenko <link.by.artem@gmail.com>
2024-04-07 19:03:37 +0300

    fixed solution

--- app/main.py
+++ app/main.py
@@ -1,35 +1,37 @@
+from __future__ import annotations
+
 from abc import ABC
-from typing import Any
 
 
 class IntegerRange:
-    def __init__(self, min_amount: int, max_amount: int) -> None:
+    def __init__(
+            self,
+            min_amount: int,
+            max_amount: int
+    ) -> None:
         self.min_amount = min_amount
         self.max_amount = max_amount
 
-    def __get__(self, instance: dict, owner: Any) -> Any:
-        return instance.__dict__[self.name]
+    def __set_name__(self, owner: object, name: str) -> None:
+        self.protected_name = "_" + name
 
-    def __set__(self, instance: dict, value: int) -> None:
-        if not isinstance(value, int):
-            raise ValueError(f"{self.name} must be an integer.")
-        if not (self.min_amount <= value <= self.max_amount):
-            raise ValueError(
-                f"{self.name} must be between {self.min_amount}"
-                f" and {self.max_amount}."
-            )
-        instance.__dict__[self.name] = value
+    def __get__(self, instance: object, owner: object) -> object:
+        return getattr(instance, self.protected_name)
 
-    def __set_name__(self, owner: Any, name: str) -> None:
-        self.name = name
+    def __set__(self, instance: object, value: int) -> None:
+        if not self.min_amount <= value <= self.max_amount:
+            raise ValueError
+        setattr(instance, self.protected_name, value)
 
 
 class Visitor:
-    age = IntegerRange(0, 100)
-    weight = IntegerRange(0, 200)
-    height = IntegerRange(0, 300)
-
-    def __init__(self, name: str, age: int, weight: int, height: int) -> None:
+    def __init__(
+            self,
+            name: str,
+            age: int,
+            weight: int,
+            height: int
+    ) -> None:
         self.name = name
         self.age = age
         self.weight = weight
@@ -37,38 +39,46 @@ class Visitor:
 
 
 class SlideLimitationValidator(ABC):
-    def __init__(self,
-                 age: int = None,
-                 weight: int = None,
-                 height: int = None
-                 ) -> None:
+    def __init__(
+            self,
+            age: int,
+            weight: int,
+            height: int
+    ) -> None:
         self.age = age
         self.weight = weight
         self.height = height
 
 
 class ChildrenSlideLimitationValidator(SlideLimitationValidator):
-    @staticmethod
-    def can_access(visitor: Visitor) -> bool:
-        age_range = 4 <= visitor.age <= 14
-        height_range = 80 <= visitor.height <= 120
-        weight_range = 20 <= visitor.weight <= 50
-        return age_range and height_range and weight_range
+    age = IntegerRange(4, 14)
+    height = IntegerRange(80, 120)
+    weight = IntegerRange(20, 50)
 
 
 class AdultSlideLimitationValidator(SlideLimitationValidator):
-    @staticmethod
-    def can_access(visitor: Visitor) -> bool:
-        age_range = 14 <= visitor.age <= 60
-        height_range = 120 <= visitor.height <= 220
-        weight_range = 50 <= visitor.weight <= 120
-        return age_range and height_range and weight_range
+    age = IntegerRange(14, 60)
+    height = IntegerRange(120, 220)
+    weight = IntegerRange(50, 120)
 
 
 class Slide:
-    def __init__(self, name: str, limitation_class: Any) -> None:
+
+    def __init__(
+            self,
+            name: str,
+            limitation_class: type[SlideLimitationValidator]
+    ) -> None:
         self.name = name
-        self.limitation_validator = limitation_class()
+        self.limitation_class = limitation_class
 
     def can_access(self, visitor: Visitor) -> bool:
-        return self.limitation_validator.can_access(visitor)
+        try:
+            self.limitation_class(
+                age=visitor.age,
+                weight=visitor.weight,
+                height=visitor.height
+            )
+        except ValueError:
+            return False
+        return True
