98333d2a2a5c5f57f105cc005e7439e3bd2cfa5f
Olexandr Bastov <iamlucky1990@gmail.com>
2024-07-28 17:18:03 +0300

    Honestly, I don't understand how to make this task some other way, but I'm sure the current solution is wrong. I need to "chain" these validators and descriptors differently. Moreover, I have used the IntegerRange descriptor in two child classes but not in the parent one, which could be more laconic and without code repeating. And validate() methods are the same in both child classes....

--- app/main.py
+++ app/main.py
@@ -1,25 +1,84 @@
-from abc import ABC
+from __future__ import annotations
+
+from abc import ABC, abstractmethod
 
 
 class IntegerRange:
-    pass
+    def __init__(self, min_amount: int, max_amount: int) -> None:
+        self.min_amount = min_amount
+        self.max_amount = max_amount
+
+    def __set_name__(self, owner: SlideLimitationValidator, name: str) -> None:
+        self.protected_name = "_" + name
+
+    def __get__(
+        self,
+        instance: SlideLimitationValidator,
+        owner: SlideLimitationValidator
+    ) -> int:
+        return getattr(instance, self.protected_name)
+
+    def __set__(self, instance: SlideLimitationValidator, value: int) -> None:
+        if not (isinstance(value, int)
+                and self.min_amount <= value <= self.max_amount):
+            value = None
+        setattr(instance, self.protected_name, value)
 
 
 class Visitor:
-   pass
+    def __init__(self, name: str, age: int, height: int, weight: int) -> None:
+        self.name = name
+        self.age = age
+        self.height = height
+        self.weight = weight
 
 
 class SlideLimitationValidator(ABC):
-    pass
+    def __init__(self, age: int, height: int, weight: int) -> None:
+        self.age = age
+        self.height = height
+        self.weight = weight
+
+    @abstractmethod
+    def validate(self) -> None:
+        pass
 
 
 class ChildrenSlideLimitationValidator(SlideLimitationValidator):
-    pass
+    age = IntegerRange(4, 14)
+    height = IntegerRange(80, 120)
+    weight = IntegerRange(20, 50)
+
+    def validate(self) -> bool:
+        if self.age and self.weight and self.height:
+            return True
+        return False
 
 
 class AdultSlideLimitationValidator(SlideLimitationValidator):
-    pass
+    age = IntegerRange(14, 60)
+    height = IntegerRange(120, 220)
+    weight = IntegerRange(50, 120)
+
+    def validate(self) -> bool:
+        if self.age and self.height and self.weight:
+            return True
+        return False
 
 
 class Slide:
-    pass
+    def __init__(
+        self,
+        name: str,
+        limitation_class: type
+    ) -> None:
+        self.name = name
+        self.limitation_class = limitation_class
+
+    def can_access(self, visitor: Visitor) -> bool:
+        validator = self.limitation_class(
+            visitor.age,
+            visitor.height,
+            visitor.weight
+        )
+        return validator.validate()
