c2c781d325d8ed701bfcdd0b94815ea931e2c10f
Olexandr Bastov <iamlucky1990@gmail.com>
2024-07-30 16:36:25 +0300

    SlideLimitationValidator()    <- the are no more brackets

--- app/main.py
+++ app/main.py
@@ -62,7 +62,7 @@ class Slide:
     def __init__(
         self,
         name: str,
-        limitation_class: SlideLimitationValidator()
+        limitation_class: SlideLimitationValidator
     ) -> None:
         self.name = name
         self.limitation_class = limitation_class
