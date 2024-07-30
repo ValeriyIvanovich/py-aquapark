34bcf639043e2942151ce51b2adba04dc702a2ee
maksym_korniev <maksymkorniev88@gmail.com>
2024-07-30 13:12:00 +0300

    refactored code styling

--- app/main.py
+++ app/main.py
@@ -60,16 +60,20 @@ class AdultSlideLimitationValidator(SlideLimitationValidator):
 
 
 class Slide:
-    def __init__(self, name: str,
-                 limitation_class: SlideLimitationValidator) -> None:
+    def __init__(
+            self, name: str,
+            limitation_class: SlideLimitationValidator
+    ) -> None:
         self.name = name
         self.limitation_class = limitation_class
 
     def can_access(self, visitor: Visitor) -> bool:
         try:
-            self.limitation_class(age=visitor.age,
-                                  weight=visitor.weight,
-                                  height=visitor.height)
+            self.limitation_class(
+                age=visitor.age,
+                weight=visitor.weight,
+                height=visitor.height
+            )
         except ValueError:
             return False
 
