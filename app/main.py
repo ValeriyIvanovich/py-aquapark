68a1eb7bf7b0e4d6e6b014a3c4cb8672916e8acd
yura <pollo7964132@gmail.com>
2024-09-14 11:09:11 +0300

    add Type annotation

--- app/main.py
+++ app/main.py
@@ -1,5 +1,6 @@
 from __future__ import annotations
 from abc import ABC
+from typing import Type
 
 
 class IntegerRange:
@@ -70,7 +71,7 @@ class AdultSlideLimitationValidator(SlideLimitationValidator):
 class Slide:
     def __init__(
             self, name: str,
-            limitation_class: SlideLimitationValidator
+            limitation_class: Type[SlideLimitationValidator]
     ) -> None:
         self.name = name
         self.limitation_class = limitation_class
