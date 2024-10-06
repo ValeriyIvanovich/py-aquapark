543174f355f7da47f7822294385da28e6b21101f
Serhiy Dema <sergeydema@gmail.com>
2024-10-06 23:21:16 +0300

    Solution

--- app/main.py
+++ app/main.py
@@ -1,25 +1,68 @@
+from __future__ import annotations
 from abc import ABC
+from typing import Any, Type
 
 
 class IntegerRange:
-    pass
+    def __init__(self, min_amount: int, max_amount: int) -> None:
+        self.min_amount = min_amount
+        self.max_amount = max_amount
+
+    def __set_name__(self, owner: Any, name: str) -> None:
+        self.private_name = "_" + name
+
+    def __get__(self, instance: object, owner: Any) -> int:
+        return getattr(instance, self.private_name)
+
+    def __set__(self, instance: object, value: int) -> None:
+        if not (self.min_amount <= value <= self.max_amount):
+            value = None
+        setattr(instance, self.private_name, value)
 
 
 class Visitor:
-   pass
+    def __init__(self, name: str, age: int, weight: int, height: int) -> None:
+        self.name = name
+        self.age = age
+        self.weight = weight
+        self.height = height
 
 
 class SlideLimitationValidator(ABC):
-    pass
+    age = None
+    height = None
+    weight = None
 
 
 class ChildrenSlideLimitationValidator(SlideLimitationValidator):
-    pass
+    age = IntegerRange(4, 14)
+    height = IntegerRange(80, 120)
+    weight = IntegerRange(20, 50)
 
 
 class AdultSlideLimitationValidator(SlideLimitationValidator):
-    pass
+    age = IntegerRange(14, 60)
+    height = IntegerRange(120, 220)
+    weight = IntegerRange(50, 120)
 
 
 class Slide:
-    pass
+    def __init__(self, name: str,
+                 limitation_class: Type[ChildrenSlideLimitationValidator
+                                        | AdultSlideLimitationValidator]
+                 ) -> None:
+        self.name = name
+        self.limitation_class = limitation_class
+
+    def can_access(self, visitor: Visitor) -> bool:
+        if not issubclass(self.limitation_class, SlideLimitationValidator):
+            return False
+        validate_instance = self.limitation_class()
+
+        validate_instance.age = visitor.age
+        validate_instance.height = visitor.height
+        validate_instance.weight = visitor.weight
+
+        return bool(validate_instance.age
+                    and validate_instance.height
+                    and validate_instance.weight)
