7bcc74f876570a7873ae4e720635be85a132701e
Serhii <serhijkrasa64uk5@gmail.com>
2024-10-14 14:13:37 +0300

    solution 5

--- app/main.py
+++ app/main.py
@@ -17,7 +17,9 @@ class IntegerRange:
         if not isinstance(value, int):
             raise TypeError(f"{value} should be integer!")
         if not (self.min_amount <= value <= self.max_amount):
-            raise ValueError(f"{value} should be between {self.min_amount} and {self.max_amount}")
+            raise ValueError(f"{value} should be between "
+                             f"{self.min_amount} and "
+                             f"{self.max_amount}")
         setattr(instance, self.protected_name, value)
 
 
